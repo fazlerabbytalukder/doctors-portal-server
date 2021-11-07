862fc51a81c16aba0ac85a2c1e0bb86736c86491
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-11-07 12:20:53 +0600

    finish

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const admin = require("firebase-admin");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// doctors-portal-firebase-adminsdk.json 
+
+
+const serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 
 
@@ -17,12 +27,27 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 // console.log(uri);
 
+async function verifyToken(req, res, next) {
+    if (req.headers.authorization.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
         await client.connect();
         const database = client.db("doctors_portal");
         const appointmentsCollection = database.collection("appointments");
+        const usersCollection = database.collection("users");
 
         //POST APPOINTMENT DATA
         app.post('/appointments', async (req, res) => {
@@ -33,18 +58,71 @@ async function run() {
 
             const appointment = req.body;
             const result = await appointmentsCollection.insertOne(appointment);
-            console.log(result);
+            // console.log(result);
             res.json(result)
         })
         //GET APPOINTMENT SPECIFIC USER DATA
-        app.get('/appointments', async (req, res) => {
+        app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
             // console.log(date);
-            const query = {email:email, date:date}
+            const query = { email: email, date: date }
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
             res.json(appointments);
+        })
+        //USER INFO POST TO THE DATABASE
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result)
+        })
+        //USER PUT FOR GOOGLE SIGN IN METHOD(upsert)
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        })
+        //ADMIN AND NORMAL USER ROLE DEFINED
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({message:'you do not have permission to make admin'})
+            }
+            
+        })
+        //DIFFERENTIATE ADMIN CAN ONLY ADD ADMIN
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            let isAdmin = false;
+            if (user) {
+                if (user.role === 'admin') {
+                    isAdmin = true;
+                }
+                res.json({ admin: isAdmin });
+            }
+            else {
+                res.json({ admin: isAdmin });
+            }
+            // res.json('dd');
         })
 
 
